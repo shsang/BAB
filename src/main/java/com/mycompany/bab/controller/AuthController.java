79df519e5298a08780cf372fd93333b71b5b4a31
han <lkas78@naver.com>
2019-04-06 15:04:12 +0900

    Revert "Revert "Tiles""
    This reverts commit 828fbbda02d09714fc47ad3a3588e89a1f6f3757.

--- src/main/java/com/mycompany/bab/controller/AuthController.java
+++ src/main/java/com/mycompany/bab/controller/AuthController.java
@@ -11,15 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.mycompany.bab.HomeController;
-
 @Controller 
-public class AuthController {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+public class AuthController {	 
 	
 	@RequestMapping(value = "/user/login", method = RequestMethod.GET)
 	public String Login(Locale locale, Model model) {		
-		return "auth/login";  
+		return "auth/login";   
 	} 
 	@RequestMapping(value = "/user/join", method = RequestMethod.GET)
 	public String Join(Locale locale, Model model) {		
---
+++ src/main/webapp/WEB-INF/views/layout.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+
+</script>
+</head>
+<body>
+	<header>
+		<tiles:insertAttribute name="header"/>
+	</header>
+	<section>
+		<tiles:insertAttribute name="content"/>
+	</section>
+	<footer>
+		<tiles:insertAttribute name="footer"/>
+	</footer>
+</body>
+</html>
